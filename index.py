cc62197e1976b09149d29d2bbfbb02dfc449c65b
sonuDada <167795293+sonuDada@users.noreply.github.com>
2024-04-27 00:35:15 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,9 +1,7 @@
 from lxml import etree
 # Load the XML Schema
-schema_file = 'C:/Users/vishwajeetkoshti/Downloads/jobhub/book_schema.xsd'
 schema = etree.XMLSchema(etree.parse('./book_schema.xsd'))
 # Load the XML document
-xml_file = 'C:/Users/vishwajeetkoshti/Downloads/jobhub/book.xml'
 xml_doc = etree.parse('./book.xml')
 # Validate the XML document against the schema
 if schema.validate(xml_doc):
